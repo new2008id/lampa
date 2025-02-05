320901c03ee1b50e0bc06dc68515e9e3c45fb8a7
Denis <33938844+new2008id@users.noreply.github.com>
2025-02-05 12:59:26 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 (function () {
     console.log('Плагин "Добавить пункт в меню" загружен!');
-
+    console.log('Lampa.Menu:', Lampa.Menu);
     Lampa.Menu.add({
         id: 'my-custom-menu-item', // Уникальный идентификатор пункта меню
         title: 'Мой пункт меню',  // Текст, отображаемый в меню
