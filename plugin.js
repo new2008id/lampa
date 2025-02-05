9e60154a377709c0f0ba5a2ab8edba110a20c110
Denis <33938844+new2008id@users.noreply.github.com>
2025-02-05 12:41:53 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,13 @@
-(function () {
-    'use strict';
-
-    console.log("Test, starting...")
-
-})();
+export default function(Lampa) {
+    console.log('Плагин "Добавить пункт в меню" загружен!');
+  
+    Lampa.Menu.add({
+      id: 'my-custom-menu-item', // Уникальный идентификатор пункта меню
+      title: 'Мой пункт меню',  // Текст, отображаемый в меню
+      icon: 'star',  // Иконка (используйте иконки Lampa или Font Awesome)
+      onClick: () => {
+        // Код, который будет выполнен при нажатии на пункт меню
+        alert('Вы нажали на мой пункт меню!');
+      }
+    });
+  }
