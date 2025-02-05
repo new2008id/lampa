b6967aefcffdd25ff23b9759d3781055effbbbad
Denis <33938844+new2008id@users.noreply.github.com>
2025-02-05 13:06:31 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,13 +1,18 @@
 (function () {
-    console.log('Плагин "Добавить пункт в меню" загружен!');
-    console.log('Lampa.Menu:', Lampa.Menu);
-    Lampa.Menu.add({
-        id: 'my-custom-menu-item', // Уникальный идентификатор пункта меню
-        title: 'Мой пункт меню',  // Текст, отображаемый в меню
-        icon: 'star',  // Иконка (используйте иконки Lampa или Font Awesome)
-        onClick: () => {
-            // Код, который будет выполнен при нажатии на пункт меню
-            alert('Вы нажали на мой пункт меню!');
-        }
-    });
+    console.log('Плагин загружен!');
+  console.log('Lampa.Menu:', Lampa.Menu);
+
+  const menuItems = [
+    {
+      id: 'my-custom-menu-item',
+      title: 'Мой пункт меню',
+      icon: 'star',
+      onClick: () => {
+        alert('Вы нажали на мой пункт меню!');
+      }
+    }
+  ];
+
+  Lampa.Menu.init(menuItems); // Инициализируем меню с нашими пунктами
+  Lampa.Menu.render(); // Отображаем меню (возможно, это необходимо)
 })();
